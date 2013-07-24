6d76d4814b42018a43bb006772f6560ca32c1964
Juni Samos <jsamos@gmail.com>
2013-07-24 14:50:32 -0400

    bug and remove supervisor update

--- recipes/cookbooks/queue-listener/recipes/supervisor.rb
+++ recipes/cookbooks/queue-listener/recipes/supervisor.rb
@@ -3,12 +3,4 @@
 
 template "/etc/supervisor.d/sqs.conf" do
   source "sqs.erb"
-  notifies :run, "bash[reread_supervisor_config]", :immediately
-end
-
-bash "reread_supervisor_config" do
-  code <<-EOH
-    sudo supervisorctl reread
-    sudo supervisorctl update
-  EOH
 end
